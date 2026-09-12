6fbcad86fb3db049407aa88ab4dffed6a2317785
Fairuz Humaira Faiza <fairuzhumaira82@gmail.com>
2026-09-13 00:40:53 +0600

    Remove unnecessary files

--- run-backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting CivicSync Spring Boot Backend...
-cd /d "%~dp0backend"
-"C:\Program Files\JetBrains\IntelliJ IDEA Educational Edition 2022.2.2\plugins\maven\lib\maven3\bin\mvn.cmd" spring-boot:run
